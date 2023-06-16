1981a11c41c827868e07b96e271d2b14a59fa40a
ghazwa-belhassen <ghazouabelhassen02@gmail.com>
2023-06-16 16:30:46 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+
+import './App.css';
+import Navbar from './components/navbar';
+import Main from './components/Main';
+import Features from './components/Features';
+import Aside from './components/Aside';
+import Footer from './components/Footer';
+function App() {
+  return (
+    <div>
+      {""}
+      <Navbar></Navbar>
+      <Main></Main>
+      <Features></Features>
+      <Aside></Aside>
+      <Footer></Footer>
+    </div>
+  );
+}
+export default App;
